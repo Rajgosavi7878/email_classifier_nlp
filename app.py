8e0588cdfd7bd87be3909d6b65212469c062ea1d
rajgosavi <rajgosavi7878@gmail.com>
2025-04-11 15:29:27 +0530

    ignored venvemail

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import *
+import pandas as pd
+from nltk import word_tokenize
+from string import punctuation
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from pickle import load
+
+# save the model and vector
+f = open("spam_model.pkl","rb")
+model = load(f)
+f.close()
+
+f = open("spam_tf.pkl","rb")
+tf = load(f)
+f.close()
+
+ss = SnowballStemmer("english")
+sw = stopwords.words("english")
+
+# text cleaning
+
+def clean_message(txt):
+	txt = txt.lower()
+	txt = txt.replace(".","")
+	txt = word_tokenize(txt)
+	txt = [t for t in txt if t not in punctuation]
+	txt = [t for t in txt if t not in sw]
+	txt = [ss.stem(t) for t in txt]
+	txt = " ".join(txt)
+	return txt
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def home():
+	msg = "";
+	if request.method == "POST":
+		txt = request.form.get("txt")
+		ctxt = clean_message(txt)
+		print(ctxt)
+		ttxt = tf.transform([ctxt])
+		print(ttxt)
+		msg = model.predict(ttxt)
+		print(msg)
+		return render_template("index.html",msg=msg)
+	else:
+		return render_template("index.html")
+if __name__ == "__main__":
+	app.run(use_reloader=True,debug=True)
+
